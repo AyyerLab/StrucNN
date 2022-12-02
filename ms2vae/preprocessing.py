fixed:fluence scaling

ms2vae/preprocessing.py
@@ -15,15 +15,42 @@ def ave_fun(q, a,b,c):
 
 def load_data():
     '''Load object's size, 2D Intensity avgs. and corresponding orientations'''
-    with h5py.File('/home/mallabhi/StrucNN/data/MS2_10k_subset.h5', 'r') as f:
-        intens = f['intens'][:]
-    orientation = np.load('/home/mallabhi/StrucNN/data/orient_10k_subet.npy')[:]
+   # with h5py.File('/home/mallabhi/StrucNN/data/MS2_10k_subset.h5', 'r') as f:
+   #     intens = f['intens'][:1000]
+
+    with h5py.File('/home/mallabhi/StrucNN/data/MS2_set.h5', 'r') as f:
+        intens_b1 = f['intens_b1'][:150]
+        orien_b1 = f['ori_b1'][:150]
+    with h5py.File('/home/mallabhi/StrucNN/data/MS2_set.h5', 'r') as f:
+        intens_b2 = f['intens_b2'][:150]
+        orien_b2 = f['ori_b2'][:150]
+    with h5py.File('/home/mallabhi/StrucNN/data/MS2_set.h5', 'r') as f:
+        intens_b3 = f['intens_b3'][:150]
+        orien_b3 = f['ori_b3'][:150]
+    intens = np.append(intens_b1, intens_b2, axis=0)
+    intens = np.append(intens, intens_b3, axis = 0)
+
+    orientation = np.append(orien_b1, orien_b2, axis=0)
+    orientation = np.append(orientation, orien_b3, axis=0)
+    
+    #orientation = np.load('/home/mallabhi/StrucNN/data/orient_10k_subet.npy')[:1000]
     orien_new = np.zeros((len(orientation), 4))
     orien_new[:,0] = orientation[:,0]
     orien_new[:,1] = -orientation[:,1]
     orien_new[:,2] = -orientation[:,2]
     orien_new[:,3] = -orientation[:,3]
     orientation = orien_new
+
+    '''Normalizing Intensity for Background'''
+    ind_ = np.arange(intens.shape[1]) - intens.shape[1]//2
+    x_, y_ = np.meshgrid(ind_, ind_, indexing='ij')
+    intrad_ = np.sqrt(x_**2 + y_**2)
+    intens_n = np.zeros((len(intens), intens.shape[1], intens.shape[1])) 
+    intens_m = np.zeros((len(intens), intens.shape[1], intens.shape[1])) 
+    for i in range(len(intens)):
+        intens_n[i] = intens[i]-intens[i][(intrad_>150)&(intrad_<240)].mean()
+        intens_m[i] = intens_n[i]/intens_n[i][(intrad_>30)&(intrad_<140)].mean()
+    
 
     '''Normalizing Intensity vals to range: 0-1'''
     sz = intens.shape[1]
@@ -32,9 +59,10 @@ def load_data():
     y0 -= sz//2
     rad_float = np.sqrt(x0**2 + y0**2)
     rad_float[rad_float==0] = 1e-3
-    ave_2d = np.array([ave_fun(i,1.15e4,4,2.95e-5) for i in rad_float.ravel()]).reshape(sz,sz)
+    ave_2d = np.array([ave_fun(i,2.74e8,3.25,9.34e-1) for i in rad_float.ravel()]).reshape(sz,sz)
 
-    norm_intens = intens/ave_2d
+    norm_intens = intens_m/ave_2d
+    norm_intens = mask_circle(norm_intens)
     input_intens = norm_intens/np.max(norm_intens) * 0.99
     input_intens = sample_down_intens(input_intens)
 
@@ -58,7 +86,7 @@ def sample_down_intens(intens_input):
     x_orig = np.arange(-size, size+1, 1.) / size
     y_orig = np.arange(-size, size+1, 1.) / size
     lmax =50 
-    step_pix = 1.33
+    step_pix = 2
     x_new = np.arange(-lmax, lmax+1, 1) / size * step_pix
     y_new = np.arange(-lmax, lmax+1, 1) / size * step_pix
     intens_input_c = np.zeros([num_l, 2*lmax+1, 2*lmax+1])